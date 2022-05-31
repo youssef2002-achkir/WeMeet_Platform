cc859e53a259c99783665133687bb88ca4a598ab
Abdelhadi <bakhtiazerreza2000@gmail.com>
2022-05-31 16:34:30 +0100

    ff

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
 app.listen(5000, () => {
-	console.log("Server has started!")
-})
+  console.log("Server has started!");
+  console.log("Server is running");
+});
