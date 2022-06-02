e1e5b9df2e35c841d20da47459197f85633d3f30
youssef2002-achkir <96985291+youssef2002-achkir@users.noreply.github.com>
2022-06-02 14:04:32 +0000

    ff

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -1,5 +1,45 @@
+
 import React, {Componenet} from "react";
 
 class Footer extends Componenet{
     
 }
+
+import React, {Component} from "react";
+import './footer.css';
+class Footer extends Component{
+    render(){
+        return(
+            <div className="Footer">
+                <section>
+                <ul>
+                    <li>WeMeet</li>
+                    <li>Pricing</li>
+                    <li>About us</li>
+                    <li>Privacy Policy</li>
+                </ul>
+                </section>
+                <section>
+                <ul>
+                    <li>Features</li>
+                    <li>Community</li>
+                    <li>Events</li>
+                    <li>Forums</li>
+                </ul>
+                </section>
+                <section>
+                <ul>
+                <li>Explore</li>
+                <li>Meetings</li>
+                <li>Talks</li>
+                <li>Categories</li>
+                </ul>
+                </section>
+                
+            </div>
+        )
+    }
+}
+
+export default Footer;
+
